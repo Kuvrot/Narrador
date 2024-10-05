21e9650be7e2c3302702a488bd210d11c2707c2a
Kuvrot <jamessanz@outlook.com>
2024-10-05 14:48:45 -0600

    implemented the chapter selector

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const text = document.getElementById("textToConvert");
 const convertBtn = document.getElementById("convertBtn");
+const chapterSelection = document.getElementById("book-chapters");
 
 convertBtn.addEventListener('click', function () {
     const speechSynth = window.speechSynthesis;
@@ -39,4 +40,67 @@ convertBtn.addEventListener('click', function () {
         speechSynth.speak(newUtter);
         convertBtn.textContent = "Sound is Playing..."
     }
+});
+
+
+const zipInput = document.getElementById('file');
+const textOutput = document.getElementById('textToConvert');
+
+var unzippedFile;
+var fileNames = [];
+
+// Listen for the file input change event
+zipInput.addEventListener('change', function() {
+    const file = zipInput.files[0]; // Get the selected .zip file
+    if (file) {
+        const reader = new FileReader();
+        
+        reader.onload = function(e) {
+            const arrayBuffer = e.target.result;
+            console.log("xd");
+            // Use JSZip to process the zip file
+            JSZip.loadAsync(arrayBuffer).then(function(zip) {
+                // Loop through the files inside the zip
+                unzippedFile = zip;
+                zip.forEach(function (relativePath, zipEntry) {
+                    if (zipEntry.name.endsWith(".xhtml")) {
+                        fileNames.push(zipEntry.name);
+                    }
+                });
+                
+            });
+
+            fileNames.sort();
+            fileNames.forEach(function (fileName){
+                        const option = document.createElement('option');
+                        option.value = fileName;
+                        option.textContent = fileName.replace('.xhtml' , '');
+                        option.textContent = option.textContent.split('/').pop();
+                        chapterSelection.appendChild(option);
+            });
+
+        };
+
+        // Read the zip file as an ArrayBuffer
+        reader.readAsArrayBuffer(file);
+    } else {
+        textOutput.value = ''; // Clear the textarea if no file is selected
+    }
+});
+
+chapterSelection.addEventListener('change' , function () {
+
+    //Clear the output
+    textOutput.value = '';
+
+    unzippedFile.forEach(function (relativePath, zipEntry) {
+        unzippedFile.forEach(function (relativePath, zipEntry) {
+            if (zipEntry.name == chapterSelection.value) {
+                zipEntry.async("text").then(function(content) {
+                content = content.replace(/<[^>]*>/g, '');
+                textOutput.value += content.replace(zipEntry.name , '');
+                });
+            }
+        });
+    });    
 });
