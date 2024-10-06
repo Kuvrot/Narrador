266731e877e752b19bcec923470990d521664b74
Kuvrot <jamessanz@outlook.com>
2024-10-06 14:16:43 -0600

    play and stop buttons implemented

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const text = document.getElementById("textToConvert");
 const convertBtn = document.getElementById("convertBtn");
 const chapterSelection = document.getElementById("book-chapters");
 const speechSynth = window.speechSynthesis;
-
 const voiceSelect = document.getElementById("voiceSelect");
+
+let playing = false;
+
 let voices = [];
 window.addEventListener("load", (event) => {
     populateVoiceList();
@@ -29,24 +31,33 @@ function populateVoiceList() {
 
 convertBtn.addEventListener('click', function () {
     const enteredText = text.value;
-    const error = document.querySelector('.error-para');
 
     if (!speechSynth.speaking && !enteredText.trim().length) {
         error.textContent = `Nothing to Convert! 
         Enter text in the text area.`
     }
 
+    if (playing){
+        stop();
+    }
+
     if (!speechSynth.speaking && enteredText.trim().length) {
-        error.textContent = "";
         const enteredText2 = enteredText.replace(/<[^>]*>/g, '');
         const newUtter =
             new SpeechSynthesisUtterance(enteredText2);
         
         newUtter.voice = voices[voiceSelect.value];
         speechSynth.speak(newUtter);
-        convertBtn.textContent = "Sound is Playing..."
+        playing = true;
+        convertBtn.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-stop" viewBox="0 0 16 16"><path d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5z"/></svg>'
     }
 });
+
+function stop () {
+    speechSynth.cancel();
+    convertBtn.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"><path d="M10.804 8 5 4.633v6.734zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696z"/></svg>'
+    playing = false;
+}
 
 const zipInput = document.getElementById('file');
 const textOutput = document.getElementById('textToConvert');
@@ -97,13 +108,14 @@ chapterSelection.addEventListener('change' , function () {
 
     //Clear the output
     textOutput.value = '';
+    stop();
 
     unzippedFile.forEach(function (relativePath, zipEntry) {
         unzippedFile.forEach(function (relativePath, zipEntry) {
             if (zipEntry.name == chapterSelection.value) {
                 zipEntry.async("text").then(function(content) {
                 content = content.replace(/<[^>]*>/g, '');
-                textOutput.value += content.replace(zipEntry.name , '');
+                textOutput.value = content.replace(zipEntry.name , '');
                 });
             }
         });
