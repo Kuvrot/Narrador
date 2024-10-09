e9c820049abacbe694ea72f2999dac46569b9984
Kuvrot <jamessanz@outlook.com>
2024-10-08 19:29:38 -0600

    narration improved
    speak rate was reduced and now the voice reads with good rythtm the title and subtitle of the files.

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ function playSegment(segment) {
     if (segment < segments.length && playing) {
         utterance = new SpeechSynthesisUtterance(segments[segment]);
         utterance.voice = voices[voiceSelect.value];
+        utterance.rate = 0.75; //Narration speed (goes from 0.0 to 1.0)
 
         //When the speech ends this is called
         utterance.onend = function () {
@@ -87,7 +88,6 @@ function stop() {
     convertBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"><path d="M10.804 8 5 4.633v6.734zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696z"/></svg>';
     playing = false;
 }
-
 
 //////// UNZIP the EPUB Book
 
